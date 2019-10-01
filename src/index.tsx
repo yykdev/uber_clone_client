8553f296372e2256c8bdef7e93fb5d745d61c494
yykdev <assavictory@gmail.com>
2019-10-01 15:49:24 +0900

    apollo.ts 에 셋팅 된 ApolloClient를 기본 App 컴포넌트에 셋팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
+import client from "./apollo";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById("root") as HTMLElement
+);
