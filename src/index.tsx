e1ecb3c7b990290f6367b6e91d3556f2314291af
yykdev <assavictory@gmail.com>
2019-10-01 16:07:22 +0900

    import 정렬 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { ApolloProvider } from "react-apollo";
+import ReactDOM from "react-dom";
 import client from "./apollo";
 import App from "./App";
 
