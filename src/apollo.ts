4781a17c28028791f0e665e542c22df807b7ae32
yykdev <assavictory@gmail.com>
2019-10-01 17:05:28 +0900

    ApolloClient clientState 셋팅

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,13 +1,49 @@
 import ApolloClient, { Operation } from "apollo-boost";
 
 const client = new ApolloClient({
+    clientState: {
+        defaults: {
+            auth: {
+                __typename: "Auth",
+                isLoggedIn: Boolean(localStorage.getItem("jwt"))
+            }
+        },
+        resolvers: {
+            Mutation: {
+                logUserIn: (_, { token }, { cache }) => {
+                    localStorage.setItem("jwt", token);
+                    cache.writeData({
+                        auth: {
+                            __typename: "Auth",
+                            isLoggedIn: true
+                        }
+                    });
+                    return null;
+                },
+                logUserOut: (_, __, { cache }) => {
+                    localStorage.removeItem("jwt");
+                    cache.writeData({
+                        data: {
+                            __typename: "Auth",
+                            isLoggedIn: false
+                        }
+                    });
+                    return null;
+                }
+            }
+        }
+    },
+    // 발생하는 모든 리퀘스트 마다 동작 할 설정을 셋팅
     request: async (operation: Operation) => {
+        // 발생하는 리퀘스트 마다 Context 를 셋팅하고 Context에 headers로 토큰을 담아 전송한다.
         operation.setContext({
             headers: {
                 "X-JWT": localStorage.getItem("jwt") || ""
             }
         });
     },
+
+    // ApolloClient 의 기본 옵션
     uri: "http://localhost:4000/graphql"
 });
 
